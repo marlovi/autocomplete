ace637cf95ef821bf50a43a6af4bec2dca73b71a
oinetsrv <oinetsrv@users.noreply.github.com>
2017-08-21 12:53:02 -0300

    ajuste banco de dados

--- projeto/php/cliente/pesquisarclientepesagemmanual.php
+++ projeto/php/cliente/pesquisarclientepesagemmanual.php
@@ -6,8 +6,28 @@
 	    $postdata = file_get_contents("php://input");
 		$request = json_decode($postdata);
 
+		$pesquisa = $dao->buscar($request->nome);			
+ 		
+		echo json_encode($pesquisa,true);
+
+
+/*
+ORIGINAL
+<?php 
+	require_once "cliente.class.php";
+	require_once "daocliente.class.php";
+ 
+		$dao = new DaoCliente();		
+	    $postdata = file_get_contents("php://input");
+		$request = json_decode($postdata);
+
 		$pesquisa = $dao->buscar();			
  		
 		echo json_encode($pesquisa,true);
 
 ?>
+
+*/
+?>
+
+
