43d2257a4e98a074b96de934c1e4c0f6d70a8e44
oinetsrv <oinetsrv@users.noreply.github.com>
2017-11-09 13:00:13 -0200

    BUG - REGISTRO COM O NOME REPETIDO
    IMPLEMENTADO CONSULTA PARA IMPEDIR REGISTRO DE PRODUTO COM  O MESMO NOME

--- projeto/controller/cadastroproduto.js
+++ projeto/controller/cadastroproduto.js
@@ -1,11 +1,65 @@
  function cadastroProdutoController($scope, $http) {
 
+$scope.verificarNomeProduto = function(nome) {
+   
+//// estou passando o nome só pra 
+// evitar o erro quando apaga totalmente
+// o campo de cadastro. 
+          if ($scope.produto.nome.length === 0){
+              
+            console.log("considerou nome length 0");
+          }
+          else {
+              
+             var request = $http({
+                  method: "post",
+                  url: "php/produto/pesquisarprodutonome.php",
+                  data: $scope.produto,
+                  headers: {
+                      'Content-Type': 'application/x-www-form-urlencoded'
+                  }
+              });
+
+              request.then(function(response) {
+                 
+                  if (response.data.status == 0) {
+                    $scope.produto.status = response.data.status;
+                    //console.log( "não existe no bancos"); 
+                   // console.log("dentro do status   " + $scope.produto.status);
+                   // console.log("dentro do  produto.nome " + $scope.produto.nome);
+
+                  } else {
+                    nome = nome.toUpperCase(); // colocando em maiusculo pq do banco so vem maiusculo.
+                   // console.log(nome);
+                   // console.log( response.data[0].nome);
+                     
+
+                      if (response.data[0].nome == nome) {
+                        Materialize.toast('PRODUTO JÁ CADASTRADO', 3000,'rounded', 'center');
+                        Materialize.toast();
+                        $scope.produto.status = 1;
+                      } else{
+                        $scope.produto.status = 0;
+                      }
+
+                  }
+
+
+              }, function(response) {
+                  console.log("ERROR" + response);
+              });
+
+} // fim do else
+          }
+
+  
+///// teste da funçaõ perder o foco do input
+$scope.focus = function() {
+    console.log("dsadsa");
+}
+
 
      $scope.salvar = function() {
-
-   
-// duvidas nessa função com a informação do data
-
         var request = $http({
             method: "post",
             url: "php/produto/salvarproduto.php",
@@ -14,7 +68,6 @@
                 'Content-Type': 'application/x-www-form-urlencoded'
             }
         });
-
         request.then(function(response) {
             console.log(response.data);
             // $scope.Cliente = response.data;
@@ -23,22 +76,13 @@
 
             $scope.produto = response.data;
                  $scope.produto = null;
-                  
 
-            //angular.forEach()
         }, function(response) {
             console.log("ERROR" + response);
         });
-//TENTANDO LIMPAR A TELA DEPOIS DE SALVO OS DADOS
-
     }
 
-    
 
-
-
-
-    /////////////////////////////////////////////////////////////
     $scope.enviar = function() {
         var req = {
             method: 'post',
@@ -48,7 +92,6 @@
             },
             data: $scope.produto
         }
-
         $http(req).then(function successCallback(response) {
             // this callback will be called asynchronously
             // when the response is available
@@ -60,23 +103,7 @@
             console.log(response);
             console.log("fail");
         });
-
     }
-
-   
-
-
-
-
-
-    $scope.voltar = function($t) {
-      //  console.log("até aqui foi");
-        //$cookieStore.put ('teste',$t);
-
-        //window.location.href='http://localhost/projeto/EMPRESA.html';
-
-    }
-
 }
 angular
     .module('home')
